7c53f35bff4dabe6cbf7526901e63a147edb2819
mrwang90hou <mrwang90hou@163.com>
2018-05-26 04:17:46 +0800

    错误【MemoryStream存储图片时写入参数失败】

--- 0713将文字保存成图片/Program.cs
+++ 0713将文字保存成图片/Program.cs
@@ -15,57 +15,71 @@ namespace _0713将文字保存成图片
     {
         static void Main(string[] args)
         {
-            Form1 f1 = new Form1();
-            f1.ShowDialog();
+            //Form1 f1 = new Form1();
+            //f1.ShowDialog();
             Program ct = new Program();
-            //string ft = "";
-            //string connectionString = "server=local;Uid=sa;password=qwe123!@#;Database=index_trademark";//server处可以为localhost（本机的MySQL），//可以为云主机，那么等于号后为ip.Database为你的数据库名称
-            ////SqlConnection conn = new SqlConnection("Data Source=(192.168.0.232);Initial Catalog=index_trademark;Integrated Security=SSPI;");
-            //SqlConnection conn = new SqlConnection(connectionString);
-            //conn.Open();//打开数据库  
-            ////创建数据库命令  
-            //SqlCommand cmd = conn.CreateCommand();
-            ////创建查询语句  
-            ////cmd.CommandText = "SELECT distinct un_simplified FROM Chinese_Img where id=22";
-            //cmd.CommandText = "SELECT distinct words FROM Chinese_words where id<=100";
-            ////从数据库中读取数据流存入reader中
-            //SqlDataReader reader = cmd.ExecuteReader();
 
-            ////从reader中读取下一行数据,如果没有数据,reader.Read()返回flase  
-            //while (reader.Read())
-            //{
-            //    //string un = reader.GetString(reader.GetOrdinal("un_simplified")).Trim();
-            //    string un = reader.GetString(reader.GetOrdinal("words")).Trim();
+            string ft = "";
+            string connectionString = "server=MRWANG90HOU;Uid=sa;password=qwe123!@#;Database=index_trademark_wn";//server处可以为localhost（本机的MySQL），//可以为云主机，那么等于号后为ip.Database为你的数据库名称
+            //SqlConnection conn = new SqlConnection("Data Source=(192.168.0.232);Initial Catalog=index_trademark;Integrated Security=SSPI;");
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();//打开数据库  
+            //创建数据库命令  
+            SqlCommand cmd = conn.CreateCommand();
+            //创建查询语句  
+            //cmd.CommandText = "SELECT distinct un_simplified FROM Chinese_Img where id=22";
+            //cmd.CommandText = "SELECT distinct words FROM Chinese_words_new where id >= 3535 and id <= 3540";//希腊字母
+            cmd.CommandText = "SELECT distinct words FROM Chinese_words_new where id =103220";//【横眉冷对千夫指  俯首甘为孺子牛】
+            //从数据库中读取数据流存入reader中
+            SqlDataReader reader = cmd.ExecuteReader();
 
-            //    for (int i = 1; i <= 2; i++)
-            //    {
-            //        switch (i)
-            //        {
-            //            case 1: ft = "黑体"; break;
-            //            case 2: ft = "宋体"; break;
-            //        }
-            //        ct.CreateImage(un, ft);  //生成图片
-            //        Console.WriteLine("保存{0}成功！", un);
-            //    }
-            //}
-            //conn.Close();
-            //Console.WriteLine("执行完毕！");
-            ct.saveToSQL();
+            //从reader中读取下一行数据,如果没有数据,reader.Read()返回flase  
+            while (reader.Read())
+            {
+                //string un = reader.GetString(reader.GetOrdinal("un_simplified")).Trim();
+                string un = reader.GetString(reader.GetOrdinal("words")).Trim();
+                
+
+
+                for (int i = 1; i <= 2; i++)
+                {
+                    switch (i)
+                    {
+                        case 1: ft = "黑体"; break;
+                        case 2: ft = "宋体"; break;
+                    }
+                    //增加对词典库中内容的判断：（1）文字个数（2）是否含有特殊字符
+                    //if (un.Length <= 4)
+                    //{
+                    //    ct.CreateImage(un, ft);  //生成图片
+                    //    //MessageBox.Show(un.Length.ToString());
+                    //}
+
+                    ct.CreateImage(un, ft);  //生成图片
+                    ct.saveToSQL(un, ft, ct.CreateImage(un, ft));  //将image图片存入数据库
+
+                    Console.WriteLine("保存{0}成功！", un);
+                }
+            }
+            conn.Close();
+            Console.WriteLine("执行完毕！");
+            
+            //ct.saveToSQL();
         }
         /// <summary>
         /// 将image图片存入数据库
         /// </summary>
         /// <param name="str">Unicode编码字符串</param>
         /// <returns>汉字字符串</returns>
-        private void saveToSQL()
+        private void saveToSQL(string content, string ft, Bitmap image)
         {
-            try
-            {
+            //try
+            //{
                 //string connString = "Data Source = . ;Initial Catalog =hotel;User ID=sa;Pwd=123456";//数据库连接字符串
                 //SqlConnection connection = new SqlConnection(connString);//创建connection对象
                 SqlConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=MySchool;Integrated Security=SSPI;");
 
-                string sql = "insert into Images (BLOBData) values (@blobdata)";
+                string sql = "insert into Images (words,words_image,fonts) values (@words,@words_image,@fonts)";
 
                 Console.WriteLine("准备执行！");
                 //conn.Open();//打开数据库  
@@ -73,6 +87,28 @@ namespace _0713将文字保存成图片
                 connection.Open();
                 //创建数据库命令  
                 SqlCommand command = new SqlCommand(sql, connection);
+
+            /*********************************Bitmap => byte[]***********************************/
+            //Bitmap image = new Bitmap("test.bmp ");
+            /*
+            MemoryStream stream = new MemoryStream();
+                image.Save(stream, ImageFormat.Jpeg);
+                byte[] mapByte = new Byte[mapStream.Length];
+                mapStream.Position = 0;
+                mapStream.Read(mapByte, 0, mapByte.Length);
+
+
+                MemoryStream ms = new MemoryStream();
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] mybyte = ms.GetBuffer();  //byte[]   bytes=   ms.ToArray(); 这两句都可以，至于区别么，下面有解释
+                ms.Close();
+                */
+
+            byte[] mybyte = PhotoImageInsert(image);
+
+                MessageBox.Show(content + "\n" + ft + "\n" + mybyte);
+                /*********************************Bitmap => byte[]***********************************/
+                /*
                 //图片路径
                 string picturePath = @"D:\222\黯沮(黑体).jpg"; //注意，这里需要指定保存图片的绝对路径和图片?
 
@@ -84,30 +120,51 @@ namespace _0713将文字保存成图片
                 //读取数据
                 fs.Read(mybyte, 0, mybyte.Length);
                 fs.Close();
+                */
                 //转换成二进制数据，并保存到数据库
-                SqlParameter prm = new SqlParameter("@blobdata", SqlDbType.VarBinary, mybyte.Length, ParameterDirection.Input, false, 0, 0, null, DataRowVersion.Current, mybyte);
-                command.Parameters.Add(prm);
+                SqlParameter prm1 = new SqlParameter("@words", SqlDbType.VarBinary, mybyte.Length, ParameterDirection.Input, false, 0, 0, null, DataRowVersion.Current, content);
+                SqlParameter prm2 = new SqlParameter("@words_image", SqlDbType.VarBinary, mybyte.Length, ParameterDirection.Input, false, 0, 0, null, DataRowVersion.Current, mybyte);
+                SqlParameter prm3 = new SqlParameter("@fonts", SqlDbType.VarBinary, mybyte.Length, ParameterDirection.Input, false, 0, 0, null, DataRowVersion.Current, ft);
+
+                command.Parameters.Add(prm1);
+                command.Parameters.Add(prm1);
+                command.Parameters.Add(prm1);
                 command.ExecuteNonQuery();
                 connection.Close();
                 Console.WriteLine("执行完毕！");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("{0}！",ex.Message);
-                //MessageBox.Show(ex.Message);
-            }
-            
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //    Console.WriteLine("{0}！",ex.Message);
+            //    //MessageBox.Show(ex.Message);
+            //}
+        }
+        /// <summary>
+        /// 图片转二进制
+        /// </summary>
+        /// <param name="imgPhoto">图片对象</param>
+        /// <returns>二进制</returns>
+        public static byte[] PhotoImageInsert(System.Drawing.Bitmap imgPhoto)
+        {
+            //将Image转换成流数据，并保存为byte[]
+            MemoryStream mstream = new MemoryStream();
+            imgPhoto.Save(mstream, System.Drawing.Imaging.ImageFormat.Jpeg);
+            byte[] byData = new Byte[mstream.Length];
+            mstream.Position = 0;
+            mstream.Read(byData, 0, byData.Length);
+            mstream.Close();
+            return byData;
         }
 
-        private Image CreateImage(string content,string ft)
+        private Bitmap CreateImage(string content,string ft)
         {
             //判断字符串不等于空和null
             if (content == null || content.Trim() == String.Empty)
                 return null;
             //string ch = ToGB2312(content);
             string ch = content;
-
-
+            
             //对字符串内容长度进行判断
             //Console.WriteLine("字符串内容长度为：{0}", content.Length);
             
@@ -139,7 +196,8 @@ namespace _0713将文字保存成图片
                 g.DrawString(ch, font, new SolidBrush(Color.Black), -16, 0);
 
                 //设置保存路径
-                image.Save(@"D:\222\"+content.Replace(@"\","") + "(" + ft + ")" + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                image.Save(@"D:\333\"+content.Replace(@"\","") + "(" + ft + ")" + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
                 return image;
             }
             catch(System.ArgumentException ex)
@@ -181,5 +239,23 @@ namespace _0713将文字保存成图片
             }
             return strResult.ToString();
         }
+        //弹出图片窗体
+        //private void ShowSelectedPicture(string path)
+        //{
+        //    FileStream fs = File.OpenRead(path); //OpenRead
+        //    int filelength = 0;
+        //    filelength = (int)fs.Length; //获得文件长度 
+        //    Byte[] image = new Byte[filelength]; //建立一个字节数组 
+        //    BitmapImage bitmapImage = new BitmapImage();
+        //    bitmapImage.BeginInit();
+        //    bitmapImage.StreamSource = new MemoryStream(image);
+        //    bitmapImage.EndInit();
+        //    var pictureWindow = new PictureWindow();//自己创建的窗口
+        //    pictureWindow.myImage.Source = bitmapImage;//myImage窗口中的图片空间
+        //    //pictureWindow.myImage.Width = bitmapImage.PixelWidth;
+        //    //pictureWindow.myImage.Height = bitmapImage.PixelHeight;
+        //    pictureWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        //    pictureWindow.ShowDialog();
+        //}
     }
 }
